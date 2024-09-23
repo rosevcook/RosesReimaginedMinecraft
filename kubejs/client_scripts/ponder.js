7376d2de417bd04caa670a53f0e471d1264b6cfb
rosevcook <rose.cook969@gmail.com>
2024-09-23 23:52:22 +0100

    custom ponders start

---
+++ kubejs/client_scripts/ponder.js
@@ -0,0 +1,17 @@
+Ponder.registry((event) => {
+    event.create('farmersdelight:rope').scene('rope', 'Using Rope', (scene, util) => {
+        //scene.showStructure();
+        scene.idle(10);
+        scene.world.createEntity('environmental:yak', [2.5, 1, 2.5]);
+        scene
+	    .text(60, 'im gay', [2.0, 2.5, 2.5])
+            .placeNearTarget()
+            .attachKeyFrame();
+        scene
+            .showControls(60, [2.5, 3, 2.5], 'down')
+            .rightClick()
+            .withItem('minecraft:shears')
+            .whileSneaking()
+            .whileCTRL();
+    })
+})

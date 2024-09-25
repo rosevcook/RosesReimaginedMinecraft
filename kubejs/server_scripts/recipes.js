807bfedfa005538e7a02993f5a302ad78a0c7281
rosevcook <rose.cook969@gmail.com>
2024-09-26 00:22:55 +0100

    fix green aspen leaves recipes

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -253,6 +253,8 @@ ServerEvents.recipes(event => {
   event.shaped('minecraft:bundle', ['S', 'L'], { S: 'minecraft:string', L: 'minecraft:leather' })
   event.shaped('artifacts:obsidian_skull', ['OOO', 'OSO', ' O '], { S: 'minecraft:wither_skeleton_skull', O: 'minecraft:obsidian' })
   event.shaped('clash:rod_of_gales', ['  N', ' S ', 'T  '], { S: 'caverns_and_chasms:spinel', T: 'minecraft:stick', N: 'caverns_and_chasms:silver_nugget' })
+  event.shaped('atmospheric:green_aspen_leaves', ['LL', 'LL'], { L: 'atmospheric:green_aspen_leaf_pile' })
+  event.shapeless(Item.of('atmospheric:green_aspen_leaf_pile', 4), ['atmospheric:green_aspen_leaves'])
 
   event.smithing('artifacts:aqua_dashers', 'minecraft:leather_boots', 'minecraft:heart_of_the_sea')
   event.smithing('windswept:snow_boots', 'minecraft:leather_boots', 'caverns_and_chasms:silver_ingot')

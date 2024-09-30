ef0405dfee660a8cb3d1848567a02140cfd664e8
rosevcook <rose.cook969@gmail.com>
2024-09-30 09:03:01 +0100

    update clash translations

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -1,6 +1,6 @@
 ItemEvents.tooltip(event => {
   event.add(['minecraft:ender_eye'], "§7Locates the Ancient City")
-  event.addAdvanced('clash:rod_of_gales', (item, advanced, text) => text.add(1, Text.of("§7Hold and release R-Click to propel yourself")))
+  event.addAdvanced('clash:rod_of_gales', (item, advanced, text) => text.add(1, Text.of("§7Hold and release R-Click when grounded to propel yourself")))
   event.addAdvanced('minecraft:wooden_pickaxe', (item, advanced, text) => text.add(1, Text.of("§7Can Mine: Stone, Coal, Copper")))
   event.addAdvanced('metalworks:copper_pickaxe', (item, advanced, text) => text.add(1, Text.of("§7Can Mine: Iron, Zinc")))
   event.addAdvanced('minecraft:iron_pickaxe', (item, advanced, text) => text.add(1, Text.of("§7Can Mine: Gold, Silver, Deepslate")))

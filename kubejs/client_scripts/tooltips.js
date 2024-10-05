15939cecdbe323e6dadb8becd3bd645a4fa4ce41
rosevcook <rose.cook969@gmail.com>
2024-10-05 15:47:36 +0100

    ignitium greatblade

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -2,6 +2,7 @@ ItemEvents.tooltip(event => {
   event.add(['minecraft:ender_eye'], "§7Locates the Ancient City")
   event.add(['cataclysm:monstrous_eye'], "§7Locates the Soul Black Smith")
   event.add(['cataclysm:flame_eye'], "§7Locates the Burning Arena")
+  event.add(['clash:greatblade'], " §9+1 Staggering Infliction")
   event.addAdvanced('clash:rod_of_gales', (item, advanced, text) => text.add(1, Text.of("§7Hold and release R-Click when grounded to propel yourself")))
   event.addAdvanced('minecraft:wooden_pickaxe', (item, advanced, text) => text.add(1, Text.of("§7Can Mine: Stone, Coal, Copper")))
   event.addAdvanced('metalworks:copper_pickaxe', (item, advanced, text) => text.add(1, Text.of("§7Can Mine: Iron, Zinc")))
--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -1,5 +1,6 @@
 StartupEvents.registry('item', event => {
   event.create('silver_sheet').displayName('Silver Sheet').texture('kubejs:item/silver_sheet')
+  event.create('zinc_sheet').displayName('Zinc Sheet').texture('kubejs:item/zinc_sheet')
   event.create('copper_wire').displayName('Copper Wire').texture('kubejs:item/copper_wire')
   event.create('power_coil').displayName('Power Coil').texture('kubejs:item/power_coil')
   event.create('silver_feather').displayName('Silver Feather').texture('kubejs:item/silver_feather')

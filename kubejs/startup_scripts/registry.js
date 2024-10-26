fd513d0c9d957a332756abb7bd18badb7393e431
rosevcook <rose.cook969@gmail.com>
2024-10-26 09:52:18 +0100

    raw zinc bricks, raw silver bricks

--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -13,6 +13,14 @@ StartupEvents.registry('block', event => {
   event.create('grave_metal_plates').material('metal').displayName('Grave Metal Plates').soundType('metal').hardness(2.5).tagBlock('minecraft:mineable/pickaxe')
   event.create('grave_metal_plate_stairs', 'stairs').material('metal').displayName('Grave Metal Plate Stairs').soundType('metal').textureAll('kubejs:block/grave_metal_plates').hardness(2.5).tagBlock('minecraft:mineable/pickaxe').tagBlock('minecraft:stairs')
   event.create('grave_metal_plate_slab', 'slab').material('metal').displayName('Grave Metal Plate Slab').soundType('metal').textureAll('kubejs:block/grave_metal_plates').hardness(2.5).tagBlock('minecraft:mineable/pickaxe').tagBlock('minecraft:slabs')
+
+  event.create('raw_silver_bricks').material('metal').displayName('Raw Silver Bricks').soundType('stone').hardness(3.5).tagBlock('minecraft:mineable/pickaxe')
+  event.create('raw_silver_brick_stairs', 'stairs').material('metal').displayName('Raw Silver Brick Stairs').soundType('stone').textureAll('kubejs:block/raw_silver_bricks').hardness(3.5).tagBlock('minecraft:mineable/pickaxe').tagBlock('minecraft:stairs')
+  event.create('raw_silver_brick_slab', 'slab').material('metal').displayName('Raw Silver Slab').soundType('stone').textureAll('kubejs:block/raw_silver_bricks').hardness(3.5).tagBlock('minecraft:mineable/pickaxe').tagBlock('minecraft:slabs')
+
+  event.create('raw_zinc_bricks').material('metal').displayName('Raw Zinc Bricks').soundType('stone').hardness(3.5).tagBlock('minecraft:mineable/pickaxe')
+  event.create('raw_zinc_brick_stairs', 'stairs').material('metal').displayName('Raw Zinc Brick Stairs').soundType('stone').textureAll('kubejs:block/raw_zinc_bricks').hardness(3.5).tagBlock('minecraft:mineable/pickaxe').tagBlock('minecraft:stairs')
+  event.create('raw_zinc_brick_slab', 'slab').material('metal').displayName('Raw Zinc Slab').soundType('stone').textureAll('kubejs:block/raw_zinc_bricks').hardness(3.5).tagBlock('minecraft:mineable/pickaxe').tagBlock('minecraft:slabs')
 })
 
 

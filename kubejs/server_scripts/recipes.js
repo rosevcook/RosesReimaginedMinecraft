5d12484c0e1026b1cb53ae11cdf57ddec2d22395
rosevcook <rose.cook969@gmail.com>
2024-12-03 05:11:05 +0000

    re add bejewelled apple, crate, enchanted golden apple is now unalloyed gold apple

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -443,6 +443,7 @@ ServerEvents.recipes(event => {
   event.remove({ id: 'minecraft:light_gray_dye_from_oxeye_daisy' })
   event.remove({ id: 'minecraft:light_blue_dye_from_blue_orchid' })
   event.remove({ id: 'minecraft:white_dye_from_lily_of_the_valley' })
+  event.remove({ id: 'neapolitan:adzuki/adzuki_crate' })
 
   // edit recipes
   event.replaceOutput({ output: 'minecraft:quartz' }, 'minecraft:quartz', 'minecraft:charcoal')
@@ -667,8 +668,8 @@ ServerEvents.recipes(event => {
   event.shapeless('atmospheric:crustose_log', ['atmospheric:aspen_log', 'atmospheric:golden_growths'])
   event.shapeless('atmospheric:crustose_wood', ['atmospheric:aspen_wood', 'atmospheric:golden_growths'])
   event.shaped(Item.of('quark:framed_glass', 4), ['GI', 'IG'], { G: 'minecraft:glass', I: 'minecraft:iron_ingot' })
-  event.shaped('kubejs:bejeweled_apple_crate', ['AAA', 'AAA', 'AAA'], { A: 'minecraft:enchanted_golden_apple' })
-  event.shapeless(Item.of('minecraft:enchanted_golden_apple', 9), ['kubejs:bejeweled_apple_crate'])
+  event.shaped('neapolitan:adzuki_crate', ['AAA', 'AAA', 'AAA'], { A: 'caverns_and_chasms:bejeweled_apple' })
+  event.shapeless(Item.of('caverns_and_chasms:bejeweled_apple', 9), ['neapolitan:adzuki_crate'])
   event.shapeless('undead_unleashed:tome_of_reflection', ['undead_unleashed:lost_soul', 'minecraft:book', 'undead_unleashed:necrotic_dust', 'minecraft:ink_sac', 'quark:glow_shroom' ])
   event.shaped('supplementaries:goblet', ['N N', 'N N', ' N '], { N: 'caverns_and_chasms:silver_nugget' })
   event.shaped('supplementaries:doormat', ['HH'], { H: 'environmental:yak_hair' })

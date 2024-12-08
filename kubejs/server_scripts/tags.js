7c19ede69714f17988df4f267f573393e30e4c6e
rosevcook <rose.cook969@gmail.com>
2024-12-08 23:24:13 +0000

    remove cringe alexsmobs paintings, paintings backport

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -114,6 +114,10 @@ ServerEvents.tags('block', event => {
   })
 })
 
+ServerEvents.tags('painting_variant', event => {
+  event.remove('minecraft:placeable', 'alexsmobs:nft', 'alexsmobs:dog_poker')
+})
+
 ServerEvents.tags('entity_type', event => {
   event.add('windswept:holly_immune', 'alexsmobs:bald_eagle', 'alexsmobs:sunbird')
   event.add('atmospheric:yucca_immune', 'naturalist:giraffe', 'naturalist:lizard_tail', 'naturalist:lizard', 'alexsmobs:guster', 'alexsmobs:emu', 'alexsmobs:fly', 'alexsmobs:jerboa', 'alexsmobs:komodo_dragon', 'environmental:zebra', 'endermanoverhaul:desert_enderman', 'alexsmobs:sunbird', 'cataclysm:kobolediator', 'cataclysm:koboleton', 'cataclysm:wadjet', 'naturalist:rattlesnake', 'naturalist:rhino', 'naturalist:hippo')
